1344e1319b6c3af70b6b8d4d4f6ec7deaf43fc3f
Francisco Folli <franciscoabel2001@gmail.com>
2022-09-17 12:48:13 -0300

    dealing with response http status code models

--- src/controllers/courses/courses.controller.ts
+++ src/controllers/courses/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Req, Res } from '@nestjs/common';
 
 // decorator that define the class as a controller for nestjs knowledge
 // with this decorator, nestjs can call controller endpoints that haves requests decorators to declare the endpoints as below
@@ -9,8 +9,8 @@ export class CoursesController {
     // and we can define a route name in each request as declared below
     // in this example the route is: courses/list 
     @Get()
-    async findall(){
-        return 'Listagem de cursos'
+    async findall(@Res() response){
+        return response.status(200).send('Listagem de Cursos');
     }
 
     @Get(':id') // this ":" notation before param name is an declaration to nestjs that we need to get the dynamic parameter
@@ -21,6 +21,7 @@ export class CoursesController {
     }
 
     @Post()
+    @HttpCode(HttpStatus.NO_CONTENT) 
     // declaring a property in @Body() decorator we can call only one parameter (destructuring)
     async create(@Body() body: any){
         // Put your magic here
