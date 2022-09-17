3ad2226cc6f2599ae197c211eb6ccbe0ad78fe0c
Francisco Folli <franciscoabel2001@gmail.com>
2022-09-16 22:58:58 -0300

    knowing the controllers

--- src/controllers/courses/courses.controller.ts
+++ src/controllers/courses/courses.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, Res } from '@nestjs/common';
 
-@Controller('courses')
-export class CoursesController {}
+// decorator that define the class as a controller for nestjs knowledge
+// with this decorator, nestjs can call controller endpoints that haves requests decorators to declare the endpoints as below
+@Controller('courses') // endpoint prefix
+export class CoursesController {
+
+    // the decorator below represents the type of request that will be received
+    // and we can define a route name in each request as declared below
+    // in this example the route is: courses/list 
+    @Get('list')
+    async findall(){
+        return 'Listagem de cursos'
+    }
+
+}
