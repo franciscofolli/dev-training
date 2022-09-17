b09c579c823c3afc1dc08293a5f119f4cc3fd2a5
Francisco Folli <franciscoabel2001@gmail.com>
2022-09-17 13:11:07 -0300

    correcting project structure and adding a service to work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+import { CoursesService } from './courses/courses.service';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoursesController } from './controllers/courses/courses.controller';
+import { CoursesController } from './courses/courses.controller';
 
 @Module({
   imports: [],
   controllers: [AppController, CoursesController],
-  providers: [AppService],
+  providers: [
+    CoursesService, AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,14 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post, Req, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Put, Req, Res } from '@nestjs/common';
+import { CoursesService } from './courses.service';
 
 // decorator that define the class as a controller for nestjs knowledge
 // with this decorator, nestjs can call controller endpoints that haves requests decorators to declare the endpoints as below
 @Controller('courses') // endpoint prefix
 export class CoursesController {
+    
+    // using dependency injection to call my service through the class constructor  
+    constructor(private readonly coursesService: CoursesService) {}
+
 
     // the decorator below represents the type of request that will be received
     // and we can define a route name in each request as declared below
@@ -24,8 +29,16 @@ export class CoursesController {
     @HttpCode(HttpStatus.NO_CONTENT) 
     // declaring a property in @Body() decorator we can call only one parameter (destructuring)
     async create(@Body() body: any){
-        // Put your magic here
         return body;
     }
 
+    @Patch(':id')
+    async update(@Param('id') id: number, @Body() body: any){
+        return `Atualização de dados do curso #${id}`
+    }
+
+    @Delete(':id')
+    async remove(@Param('id') id: number){
+        return `Exclusão do C urso #${id}`;
+    }
 }
