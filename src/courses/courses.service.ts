e644ffd9151591db61e8a3be02fed859f843cdee
Francisco Folli <franciscoabel2001@gmail.com>
2022-09-17 13:24:43 -0300

    implement services methods to call using controller

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -3,8 +3,47 @@ https://docs.nestjs.com/providers#services
 */
 
 import { Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entity';
 
 @Injectable()
 export class CoursesService {
-    private courses = [];
+    private courses: Course[] = [
+        {
+            id: 1,
+            name: "Fundamentos framework NestJS",
+            description: "Fundamentos framework NestJS",
+            tags: ["nodejs","nestjs","javascript","typescript"]
+        }
+    ];
+
+    create(createCourseDto: any){
+        this.courses.push(createCourseDto);
+    }
+
+    
+    findAll(){
+        return this.courses;
+    }
+
+    findOne(id: number){
+        return this.courses.find((course: Course) => course.id === id)
+    }
+
+    
+    update(id: number, updateCourseDto: any){
+        const courseIndex: number = this.courses.findIndex(course => course.id === id);
+        this.courses[courseIndex] = updateCourseDto;
+    }
+
+    
+    remove(id: number){
+        const courseIndex: number = this.courses.findIndex(course => course.id === id);
+
+        if(courseIndex >= 0) {
+            this.courses.splice(courseIndex, 1);
+        }
+    }
+    
+
+
 }
---
+++ src/courses/entities/course.entity.ts
@@ -0,0 +1,7 @@
+// defining a ts class as an entity of module
+export class Course {
+    id: number;
+    name: string;
+    description: string;
+    tags: string[];
+}
