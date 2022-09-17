c567375ff0dbd8447b22d955d810e22a3ef07b7d
Francisco Folli <franciscoabel2001@gmail.com>
2022-09-16 23:30:36 -0300

    upload params concept and use in controller

--- src/controllers/courses/courses.controller.ts
+++ src/controllers/courses/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import { Controller, Get, Param, Req, Res } from '@nestjs/common';
 
 // decorator that define the class as a controller for nestjs knowledge
 // with this decorator, nestjs can call controller endpoints that haves requests decorators to declare the endpoints as below
@@ -8,9 +8,17 @@ export class CoursesController {
     // the decorator below represents the type of request that will be received
     // and we can define a route name in each request as declared below
     // in this example the route is: courses/list 
-    @Get('list')
+    @Get()
     async findall(){
         return 'Listagem de cursos'
+    }
+
+    @Get(':id') // this ":" notation before param name is an declaration to nestjs that we need to get the dynamic parameter
+    // with @Param decorator we can define the params that we need to receive and get they in a decorated attribute
+    // declaring a property in @Param() decorator we can call only one parameter (destructuring)
+    async findOne(@Param('id') id: number){
+        // Put your magic here
+        return `Curso #${id}`;
     }
 
 }
